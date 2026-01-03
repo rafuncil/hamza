f69fc1341c16f4da0151ecb48923cddfd6207d87
Rafael Shokuev <rafaelshokuev@MacBook-Air-Rafael.local>
2026-01-03 22:37:29 +0300

    fix url

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -35,7 +35,7 @@ export default function Home() {
 
 
   useEffect(() => {
-    fetch('https://github.com/rafuncil/hamza/blob/main/public/products_full.json')
+    fetch('https://raw.githubusercontent.com/rafuncil/hamza/main/public/products_full.json')
     .then(response => response.json())
     .then(data => setOptions(data))
     .catch(error => console.error('Error:', error));
